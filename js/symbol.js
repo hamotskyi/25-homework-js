044d95be2f624bb9391e89f06d393c5d687d0ed3
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-11-22 23:43:45 +0200

    symbol done

--- js/symbol.js
+++ js/symbol.js
@@ -1,23 +1,30 @@
-let a = prompt('Салам! Давай ти мені цихру - я тобі значок любий.');
+function tellMeNumber() {
+    let request = document.getElementById('userNumber');
+    let a = parseInt (request.value)
 
-if (a == 1) {
-    alert ('Ти мені ' + a + ' - я тобі "!"')
-} else if (a == 2) {
-    alert ('Ти мені ' + a + ' - я тобі "@"')
-} else if (a == 3) {
-    alert ('Ти мені ' + a + ' - я тобі "#"')
-} else if (a == 4) {
-    alert ('Ти мені ' + a + ' - я тобі "$"')
-} else if (a == 5) {
-    alert ('Ти мені ' + a + ' - я тобі "%"')
-} else if (a == 6) {
-    alert ('Ти мені ' + a + ' - я тобі "^"')
-} else if (a == 7) {
-    alert ('Ти мені ' + a + ' - я тобі "&"')
-} else if (a == 8) {
-    alert ('Ти мені ' + a + ' - я тобі "*"')
-} else if (a == 9) {
-    alert ('Ти мені ' + a + ' - я тобі "("')
-} else if (a == 0) {
-    alert ('Ти мені ' + a + ' - я тобі ")"')
+    if (a >= 0 && a <= 9) { 
+        if (a == 1) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "!"'
+        } else if (a == 2) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "@"'
+        } else if (a == 3) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "#"'
+        } else if (a == 4) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "$"'
+        } else if (a == 5) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "%"'
+        } else if (a == 6) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "^"'
+        } else if (a == 7) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "&"'
+        } else if (a == 8) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "*"'
+        } else if (a == 9) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі "("'
+        } else if (a == 0) {
+            document.getElementById('answer').innerHTML = 'Ти мені ' + a + ' - я тобі ")"'
+        }
+    } else {
+        document.getElementById('answer').innerHTML = 'Спробуй ше раз. В тебе вийде. Я в тебе вірю...<br>Просто натисни ОДИН раз клавішу з ЦИФРОЮ!<br>Один раз. Клавішу з цифрою.'
+    }
 }

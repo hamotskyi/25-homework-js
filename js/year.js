3e6c2dbb2852b96d2f5ec38cfd13a437f240d329
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-11-23 16:10:51 +0200

    year done

--- js/year.js
+++ js/year.js
@@ -1,15 +1,25 @@
-let year = parseInt (prompt ('Напиши якийсь рік!'));
+function tellMeYear() {
+    let request = document.getElementById('userYear');
+    let year = parseInt(request.value);
 
-if (Number.isInteger(year)) {
-    if (year >= 0) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-            alert('Високосний!');
+
+    if (Number.isInteger(year)) {
+        if (year >= 0) {
+            if (year <= 2099) {
+                
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+                    document.getElementById('answer').innerHTML = 'Високосний!';
+                } else {
+                    document.getElementById('answer').innerHTML = 'Не високосний!';
+                }
+
+            } else {
+                document.getElementById('answer').innerHTML = 'Не забігай так далеко. Бо там вже і "лампа не горит" і "врут календари"';
+            }
         } else {
-            alert('Не високосний!');
+            document.getElementById('answer').innerHTML = 'Давай ото як Ісус родився і дальше. Всьо шо раньше - не підходе';
         }
     } else {
-        alert('Давай ото як Ісус родився і дальше. Всьо шо раньше - не підходе');
+        document.getElementById('answer').innerHTML = 'Цифрами пиши';
     }
-} else {
-    alert('Цифрами пиши')
-};
+}

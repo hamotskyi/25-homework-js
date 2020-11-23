7cebe71e8c3727380e64c8de8583bc2688f117fc
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-11-23 15:06:10 +0200

    3-dig-number done

--- js/3-dig-number.js
+++ js/3-dig-number.js
@@ -1,24 +1,25 @@
-let  userNumber = parseInt (prompt ('Напиши трьохзначне число. Маєш одну попитку, чесно!'));
+function tellMeNumber() {
+    let request = document.getElementById('userNumber');
+    let userNumber = parseInt (request.value)
+    let num1 = parseInt(userNumber / 100);
+    let num2 = parseInt(userNumber / 10) % 10;
+    let num3 = userNumber % 10;
+    
+    if (Number.isInteger(userNumber)) {
 
-let num1 = parseInt(userNumber / 100);
-let num2 = parseInt(userNumber / 10) % 10;
-let num3 = userNumber % 10;
-
-if (Number.isInteger(userNumber)) {
-
-    if (99 < userNumber && userNumber < 1000) {
-
-        if (num1 === num2 || num2 === num3 || num1 === num3) {
-            alert('В цьому числі є однакові цифри')
-        } else { 
-            alert('В цьому числі всі цифри різні')
+        if (99 < userNumber && userNumber < 1000) {
+    
+            if (num1 === num2 || num2 === num3 || num1 === num3) {
+                document.getElementById('answer').innerHTML = 'В цьому числі є однакові цифри'
+            } else { 
+                document.getElementById('answer').innerHTML = 'В цьому числі всі цифри різні'
+              }
+    
+        } else {
+            document.getElementById('answer').innerHTML = '"Трьохзначне" - це число, яке складається з ТРЬОХ чисел...'
           }
-
+    
     } else {
-        alert('Попитки кончились. Прощай!')
-      }
-
-} else {
-    alert('Попитки кончились. Прощай!')
-  };
-  
+        document.getElementById('answer').innerHTML = 'Спробуй, все ж таки, число...'
+      };
+}

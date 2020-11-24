6b9ffad392f7772734b01f75e1ac2b878f9f74f4
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-11-24 21:34:08 +0200

    finaly fixed polindrome.js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,12 +1,35 @@
 function fnPalindrome() {
     let userData = document.getElementById('userText').value;
+    let userText = userData.replace(/\s+/g, '');
+    let userTextReverse = userText.split('').reverse().join('');
+    let answer = document.getElementById('answer');
+
+    if (userTextReverse == userText) {
+        return(answer.innerHTML = "О! Це поліндром!");
+    } else {
+        return(answer.innerHTML = "Це не поліндром");
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+    let userData = document.getElementById('userText').value;
     let answer = document.getElementById('answer');
     let userText = userData.replace(/\s+/g, '');
     
-    for(let i = 0; i <= userText/2; i++) {
-        if(userText[i] !== userText[userText.length - 1 - i]) {
+    for(let i = 0; i < userText.lenght; i++) {
+        if(userText[i] === userText[userText.length - 1 - i]) {
             return(answer.innerHTML = "Це не поліндром");
+        } else {
+            return(answer.innerHTML = "О! Це поліндром!");
         }
-    return(answer.innerHTML = "О! Це поліндром!"); 
     }
 };
+*/

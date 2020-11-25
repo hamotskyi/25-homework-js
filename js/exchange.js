133acc163693985918767480954bd76ef2dc9730
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-11-25 20:57:32 +0200

    exchange almost done

--- js/exchange.js
+++ js/exchange.js
@@ -1,23 +1,45 @@
-function fnExchange() {
-    let userData = document.getElementById('userMoney').value;
-    let userMoney = parseInt(userData);
-    let answer = document.getElementById('answer');
-    let list = document.getElementById('ol');
-    let radioArr = document.getElementsByName('exchange');
-
-    console.log (radioArr)
-
+function toExchange() {
+    var userData = document.getElementById('userMoney').value;
+    var userMoney = parseInt(userData);
+    var list = document.getElementById('ol');
+    var answer = document.getElementById('answer');
 
     if (userMoney <= 0) {
-        list.insertAdjacentHTML("beforeEnd", '<li><p id="answer">О нє, це дуже мало!</p></li>');
+        list.insertAdjacentHTML("beforeEnd", '<li><p id="answer">Нє друг, так діла не буде!</p></li>');
     } else {
-        list.insertAdjacentHTML("beforeEnd", '<li><p>Шо хочеш купити?</p></li><li><input id="exchangeUsd" type="radio" name="exchange" value="usd"><label for="exchangeUsd">USD</label> <input id="exchangeEur" type="radio" name="exchange" value="eur"><label for="exchangeEur">EUR</label> <input id="exchangePln" type="radio" name="exchange" value="pln"><label for="exchangePln">PLN</label></li><li><p id="answer"></p></li>');
+        list.insertAdjacentHTML("beforeEnd", '<li><p>Шо хочеш купити?</p></li><li><input onchange="toChooseCurrency()" id="exchangeUsd" type="radio" name="exchange" value="28.3"><label for="exchangeUsd">USD</label> <input onchange="toChooseCurrency()" id="exchangeEur" type="radio" name="exchange" value="33.7"><label for="exchangeEur">EUR</label> <input onchange="toChooseCurrency()" id="exchangePln" type="radio" name="exchange" value="7.5"><label for="exchangePln">PLN</label></li><li><p id="answer"></p></li>');
     }
+
+};
+
+function toChooseCurrency() {
+    var radioArr = document.getElementsByName('exchange');
+    var userData = document.getElementById('userMoney').value;
+    var userMoney = parseInt(userData);
 
     for (var i = 0; i < radioArr.length; i++) {
         if (radioArr[i].type == "radio" && radioArr[i].checked) {
-            alert("selected: " + radioArr[i].value);
-        }
+            var floorNumber = Math.floor(userMoney / radioArr[i].value);
+            var moneyChange = (userMoney - (floorNumber * radioArr[i].value)).toFixed(0);
+
+            if (radioArr[i].value == 28.3) {
+                var symbol = '$'
+            } else if (radioArr[i].value == 33.7) {
+                var symbol = '€'
+            } else {
+                var symbol = 'zł'
+            };
+
+            if (userMoney <= radioArr[i].value) {
+                answer.innerHTML = 'Це получається ' + (userMoney / radioArr[i].value).toFixed(2) + symbol + '. Але мєлочью я не торгую!';
+            } else if (userMoney % radioArr[i].value = 0) {                             //TOFINISH
+                answer.innerHTML = 'Це получається ' + floorNumber + symbol;
+            } else {
+                answer.innerHTML = 'Це получається ' + floorNumber + symbol + ' і ' + moneyChange + 'грн здачі.';
+            }
+        
+            break;
+        } 
     }
 
-}
+};

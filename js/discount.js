8065006f77c5f9387414335881f8b74be4745b1d
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-12-02 14:13:41 +0200

    discount done

--- js/discount.js
+++ js/discount.js
@@ -1,9 +1,30 @@
 function fnDiscount () {
     let money = Number(document.getElementById('userMoney').value).toFixed(2);
+    let list = document.getElementById('ol');
+    let three = (money - (money * 3 / 100)).toFixed(2);
+    let five = (money - (money * 5 / 100)).toFixed(2);
+    let seven = (money - (money * 7 / 100)).toFixed(2);
+    let answer = document.getElementById('answer');
 
+
+    if (money <= 0) {
+        answer.innerHTML = 'Давай не чуди! Шо там по сумі?';
+    } else if (isNaN(money)) {
+        answer.innerHTML = 'Давай не чуди! Шо там по сумі?';
+    } else if (money > 0 && money <= 199) {
+        answer.innerHTML = 'Маловато для скидки';
+    } else if (money >= 200 && money <= 299) {
+        answer.innerHTML = `Твоя скидка - 3%!<br>Сумма зі скидкою - ${three}`;
+    } else if (money >= 300 && money <= 499) {
+        answer.innerHTML = `Твоя скидка - 5%!<br>Сумма зі скидкою - ${five}`;
+    } else if (money >= 500) {
+        answer.innerHTML = `Твоя скидка - 7%!<br>Сумма зі скидкою - ${seven}`;
+    }
 
     console.log (money);
     console.log (typeof (money));
+    console.log (three);
+
 
 
 } 

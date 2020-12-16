b91f8cbb65abd77934505109485ef38f1786d633
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-12-16 18:48:49 +0200

    date done

--- js/date.js
+++ js/date.js
@@ -1,20 +1,18 @@
 function showNextDate() {
-    let userDate = document.getElementById("userDate").value;
-    
-    console.log(document.getElementById("userDate").value)
 
-    let usersDateNumb = new Date(userDate).getTime() + (1000*60*60*24);
-    
-    console.log(usersDateNumb)
+    let userData = document.getElementById("userDate").value;
+    let userDate = new Date(userData).getTime();
+    let today = new Date(userData);
+    let yearNow = today.getFullYear();
+    let monthNow = today.getMonth()+1;
+    let dayNow = today.getDate();
+    let nextDate = userDate + (1000*60*60*24);
+    let tomorrow = new Date(nextDate);
+    let yearNext = tomorrow.getFullYear();
+    let monthNext = tomorrow.getMonth()+1;
+    let dayNext = tomorrow.getDate();
+    let anwser = `Після ${dayNow}.${monthNow}.${yearNow} буде ${dayNext}.${monthNext}.${yearNext}`;
 
-    let tomorrow = new Date(usersDateNumb);
-    console.log(tomorrow.getMonth())
-    let yearTomor = tomorrow.getFullYear();
-    let monthTomor = tomorrow.getMonth()+1;
-    let dayTomor = tomorrow.getDate();
-    
-    let tomorrowStr = `The next day after ${userDate} is ${yearTomor}-${monthTomor}-${dayTomor}`;
-
-    document.getElementById("answer").innerHTML = tomorrowStr;
+    document.getElementById("answer").innerHTML = anwser;
 
 }

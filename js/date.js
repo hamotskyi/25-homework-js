40d7852119219c18489842433d1d3b2c5d4c71c4
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2020-12-17 20:06:11 +0200

    shopinglist init

--- js/date.js
+++ js/date.js
@@ -2,16 +2,13 @@ function showNextDate() {
 
     let userData = document.getElementById("userDate").value;
     let userDate = new Date(userData).getTime();
-    let today = new Date(userData);
-    let yearNow = today.getFullYear();
-    let monthNow = today.getMonth()+1;
-    let dayNow = today.getDate();
     let nextDate = userDate + (1000*60*60*24);
     let tomorrow = new Date(nextDate);
     let yearNext = tomorrow.getFullYear();
     let monthNext = tomorrow.getMonth()+1;
     let dayNext = tomorrow.getDate();
-    let anwser = `Після ${dayNow}.${monthNow}.${yearNow} буде ${dayNext}.${monthNext}.${yearNext}`;
+    
+    let anwser = `А далі буде ${dayNext}.${monthNext}.${yearNext}`;
 
     document.getElementById("answer").innerHTML = anwser;
 
